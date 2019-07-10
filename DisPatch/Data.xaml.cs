3a7f89d00ee7c3404d2a234acad510d71a1dd56d
Server <Server@SERVER>
2019-07-10 18:12:10 +0430

    Add project files.

---
+++ DisPatch/Data.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DisPatch
+{
+    /// <summary>
+    /// Interaction logic for Data.xaml
+    /// </summary>
+    public partial class Data : Window
+    {
+        Timer timer = new Timer(1000);
+        public Data()
+        {
+            InitializeComponent();
+
+            time2TextBox.Text = "ساعت: " + DateTime.Now.ToString("HH:mm:ss");//"HH:mm:ss"
+            date2TextBox.Text = DateTime.Now.ToString("D");//"HH:mm:ss"
+                                                          // timeTextBox.Text = DateTime.Now.ToString("yyyy/MM/dd");
+            timer.Start();
+        }
+
+        private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DisPatch/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+using System.Timers;
+
+namespace DisPatch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Timer timer = new Timer(1000);
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timeTextBox.Text = "ساعت: " + DateTime.Now.ToString("HH:mm:ss");//"HH:mm:ss"
+            dateTextBox.Text = DateTime.Now.ToString("D");//"HH:mm:ss"
+                                                                 // timeTextBox.Text = DateTime.Now.ToString("yyyy/MM/dd");
+            timer.Start();
+        }
+
+        private void login(object sender, RoutedEventArgs e)
+        {
+            if (UsernameTextBox.Text.Equals("danny") && PasswordTextBox.Password.Equals("test"))
+            {
+
+                Data data = new Data();
+                this.Visibility = Visibility.Hidden;
+                data.Show();
+              //  new Data().ShowDialog();
+              //  this.Close();
+                
+                
+
+            }
+            else
+            {
+                MessageBox.Show("نام کاربری یا رمز ورود اشتباه است");
+               
+            }
+        }
+  
+
+
+
+    }
+}
