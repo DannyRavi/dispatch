cd5fbdba573d617481daa174c8b1c8e32b0f651c
DANNY <danykhosravi@gmail.com>
2019-07-15 17:57:54 +0430

    add  sync with webservice

---
+++ DisPatch/DataDownload.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisPatch
+{
+    class DataDownload
+    {
+
+
+
+        public string first_name_ { get; set; }
+        public string last_name_ { get; set; }
+        public string phone_number_ { get; set; }
+        public string mobile_number_ { get; set; }
+        public string age_ { get; set; }
+        public string group_ { get; set; }
+        public string nation_code_ { get; set; }
+        public string height_ { get; set; }
+        public string weigth_ { get; set; }
+        public string address_ { get; set; }
+        public string postal_code_ { get; set; }
+        public string email_ { get; set; }
+        public string gender_ { get; set; }
+        public string user_ { get; set; }
+        public string Name { get; set; }
+        public int Size { get; set; }
+
+        //public DataDownload(string first_name_,
+        //string last_name_,
+        //string phone_number_,
+        //string mobile_number_,
+        //string age_, string group_,
+        //string nation_code_,
+        //string height_,
+        //string weigth_,
+        //string address_,
+        //string postal_code_,
+        //string email_,
+        //string gender_,
+        //string user_)
+
+
+        //{
+        //    this.first_name_ = first_name_;
+        //    this.last_name_ = last_name_;
+        //    this.phone_number_ = phone_number_;
+        //    this.mobile_number_ = mobile_number_;
+        //    this.age_ = age_;
+        //    this.group_ = group_;
+        //    this.nation_code_ = nation_code_;
+        //    this.height_ = height_;
+        //    this.weigth_ = weigth_;
+        //    this.address_ = address_;
+        //    this.postal_code_ = postal_code_;
+        //    this.email_ = email_;
+        //    this.gender_ = gender_;
+        //    this.user_ = email_;
+        //}
+    }
+}
---
+++ DisPatch/ProfileDownload.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DisPatch
+{
+   static class ProfileDownload
+    {
+
+        public static async void DownloadPageAsync()
+        {
+            string URL = "https://dannyraviii.pythonanywhere.com/bip/profile/1/";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.ContentType = "application/json; charset=utf-8";
+            request.Headers["Authorization"] = "Token 5e95ee2a97d212e9080d7079149073b1a2ae4d7b";
+            request.PreAuthenticate = true;
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+
+                string json = string.Empty;
+                string sson = string.Empty;
+                json = reader.ReadToEnd();
+                var res = JsonConvert.DeserializeObject<dynamic>(json);
+            //    DataDownload dd = new DataDownload();
+                string first_name_ = res.first_name;
+                string last_name_ = res.first_name;
+                string phone_number_ = res.first_name;
+                string mobile_number_ = res.first_name;
+                string age_ = res.first_name;
+                string group_ = res.first_name;
+                string nation_code_ = res.first_name;
+                string height_ = res.first_name;
+                string weigth_ = res.first_name;
+                string address_ = res.first_name;
+                string postal_code_ = res.first_name;
+                string email_ = res.first_name;
+                string gender_ = res.first_name;
+                string user_ = res.first_name;
+
+            }
+        }
+    }
+}
